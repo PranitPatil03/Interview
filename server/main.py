45a5ebdea8d8b13210cbb30b020744f932c7e75d
Pranit <patilpranit3112@gmail.com>
2025-03-09 22:16:40 +0530

    added cors

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from service import read_profit_and_loss_data
 from config import config
 from typing import List
@@ -7,14 +8,23 @@ import json
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/quickbooks/profit-and-loss")
 async def read_profit_and_loss():
     file_path = config.data_file_path
     try:
+        print(file_path)
         result = read_profit_and_loss_data(file_path)
         return result
     except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="File not found")
+        raise HTTPException(status_code=404, detail="File not found1")
     except json.JSONDecodeError:
         raise HTTPException(status_code=500, detail="Error reading JSON data")
  
--- server/requirements.py
+++
@@ -1,3 +0,0 @@
-fastapi==0.95.0
-uvicorn==0.22.0
-python-dotenv==1.0.0
